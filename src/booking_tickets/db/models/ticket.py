edd8bc604d56cdd1cad33cc356f306de91f9b588
YVVV19 <vadimyankovskiy2000@gmail.com>
2024-11-04 19:33:16 +0200

    ...

--- src/booking_tickets/db/models/ticket.py
+++ src/booking_tickets/db/models/ticket.py
@@ -12,4 +12,4 @@ class Ticket(Config.BASE):
     date:Mapped[str]
     company: Mapped["Company"] = relationship(back_populates="tickets")
     company_id: Mapped[int] = mapped_column(ForeignKey("company.id"))
-    price: Decimal
+    price: Mapped[Decimal]
